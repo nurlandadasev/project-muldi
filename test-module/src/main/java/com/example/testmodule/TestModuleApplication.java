9304a994c5fd8f5cd6862876927d2fbbb86077c3
Nurlan <nurlandadasev@gmail.com>
2022-02-18 16:04:49 +0400

    Initial commit

---
+++ test-module/src/main/java/com/example/testmodule/TestModuleApplication.java
@@ -0,0 +1,21 @@
+package com.example.testmodule;
+
+import com.example.testmodulesecond.entities.Users;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestModuleApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestModuleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Users users = new Users();
+
+        System.out.println(users);
+    }
+}
